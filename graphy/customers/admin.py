21bda53cd1c51375f58cad95d1f561e77ee84408
Tomas Fagerbekk <tomas@otovo.com>
2018-11-17 00:56:13 +0100

    customers: add Customer to django admin

--- graphy/customers/admin.py
+++ graphy/customers/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from graphy.customers.models import Customer
+from graphy.utils.base_models import BaseAdmin
+
+
+@admin.register(Customer)
+class CustomerAdmin(BaseAdmin):
+    list_display = ('email', 'home_address')
+    fields = (
+        'email',
+        'phone_number',
+    )
+    raw_id_fields = ('home_address', 'user',)
