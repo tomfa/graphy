5949e557658b9700cf84164268f32fdaa47de59c
Tomas Fagerbekk <tomas@otovo.com>
2018-11-17 00:56:01 +0100

    leads: add Lead to django admin

--- graphy/leads/admin.py
+++ graphy/leads/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from graphy.leads.models import Lead
+from graphy.utils.base_models import BaseAdmin
+
+
+@admin.register(Lead)
+class LeadAdmin(BaseAdmin):
+    list_display = ('uncleaned_address', 'email')
+    fields = (
+        'email',
+        'address',
+        'uncleaned_address',
+        'utm_campaign',
+        'utm_medium',
+        'utm_source',
+    )
+    raw_id_fields = ('address',)
